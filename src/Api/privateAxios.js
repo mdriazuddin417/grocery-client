51c114834bac9bed48157b056ad5a43c76085fb9
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-05-31 07:06:56 +0600

    change

--- src/Api/privateAxios.js
+++ src/Api/privateAxios.js
@@ -2,10 +2,8 @@ import axios from "axios";
 
 const privateAxios = axios.create({});
 
-// Add a request interceptor
 privateAxios.interceptors.request.use(
   function (config) {
-    // Do something before request is sent
     if (!config.headers.authorization) {
       config.headers.authorization = `Bearer ${localStorage.getItem(
         "accessToken"
@@ -14,7 +12,6 @@ privateAxios.interceptors.request.use(
     return config;
   },
   function (error) {
-    // Do something with request error
     return Promise.reject(error);
   }
 );
@@ -22,12 +19,9 @@ privateAxios.interceptors.request.use(
 // Add a response interceptor
 privateAxios.interceptors.response.use(
   function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
     return response;
   },
   function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
     if (error.response.status === 403) {
     }
 
