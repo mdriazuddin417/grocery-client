0a4a74eec2ec879afbf3bcbe6b75bfa90b26e1ae
mdriazuddin417 <mdriazuddin417@gmail.com>
2022-05-31 07:30:13 +0600

    hi

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: "AIzaSyBNfyG6VvJgeUxeKrTdOpI3GbZRhBJkid0",
+  authDomain: "grocery-shop-e85f5.firebaseapp.com",
+  projectId: "grocery-shop-e85f5",
+  storageBucket: "grocery-shop-e85f5.appspot.com",
+  messagingSenderId: "677905503920",
+  appId: "1:677905503920:web:f937b0ba2eb66e253d8d75",
 };
 
 // Initialize Firebase
